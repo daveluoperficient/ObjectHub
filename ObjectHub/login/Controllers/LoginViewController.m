2bcd127d438bd04d09249b8ecdd36b6bb4f63923
dave.luo <dave.luo@perficient.com>
2018-10-30 13:22:03 +0800

    update login screen and app icon

--- ObjectHub/login/Controllers/LoginViewController.m
+++ ObjectHub/login/Controllers/LoginViewController.m
@@ -9,7 +9,7 @@
 #import "LoginViewController.h"
 #import <QuartzCore/QuartzCore.h>
 
-@interface LoginViewController ()
+@interface LoginViewController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *textFieldPhoneNumber;
 @property (weak, nonatomic) IBOutlet UITextField *textFieldPassword;
@@ -21,9 +21,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.view.backgroundColor = [UIColor blackColor];
-    
     [self setUpLayout];
 }
 
@@ -34,12 +31,35 @@
     [_textFieldPhoneNumber setKeyboardType:UIKeyboardTypeNumberPad];
     [_textFieldPhoneNumber setValue:[NSNumber numberWithInt:20] forKey:@"paddingLeft"];
     
+    _textFieldPhoneNumber.delegate = self;
+    
+    _textFieldPhoneNumber.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [_textFieldPhoneNumber addTarget:self action:@selector(textFieldDidChange) forControlEvents:UIControlEventEditingChanged];
+    
     _textFieldPassword.layer.cornerRadius = 20;
     _textFieldPassword.layer.masksToBounds = YES;
     _textFieldPassword.alpha = 0.8;
     [_textFieldPassword setValue:[NSNumber numberWithInt:20] forKey:@"paddingLeft"];
     
     _loginButton.layer.cornerRadius = 25;
+}
+
+#pragma delegate method
+
+-(void)textFieldDidChange {
+    NSDictionary *attrsDictionary =@{
+                                     NSFontAttributeName: _textFieldPhoneNumber.font,
+                                     NSKernAttributeName:[NSNumber numberWithFloat:2.0f]
+                                     };
+    _textFieldPhoneNumber.attributedText=[[NSAttributedString alloc] initWithString:_textFieldPhoneNumber.text attributes:attrsDictionary];
+    
+    if (_textFieldPhoneNumber.text.length > 11) {
+        _textFieldPhoneNumber.text = [_textFieldPhoneNumber.text substringToIndex:11];
+    }
+}
+
+-(BOOL)textFieldShouldClear:(UITextField *)textField {
+    return YES;
 }
 
 @end
