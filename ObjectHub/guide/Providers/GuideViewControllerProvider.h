220073494c05b39e4289a486766bcdb5a9276e6a
dave.luo <dave.luo@perficient.com>
2018-12-01 14:23:26 +0800

    Set guide page with entrance and modify guide images

--- ObjectHub/guide/Providers/GuideViewControllerProvider.h
+++ ObjectHub/guide/Providers/GuideViewControllerProvider.h
@@ -1,11 +1,3 @@
-//
-//  GuideViewControllerProvider.h
-//  ObjectHub
-//
-//  Created by lester.ji on 2018/11/1.
-//  Copyright © 2018年 dave.luo. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @class GuideViewController;
